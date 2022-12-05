2e1bc60e93f531910db6697f86754a2ecc9204e5
mica <micaarbolito94@gmail.com>
2022-12-05 03:29:55 -0300

    clase 05 completada

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-( () => {
+
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -18,14 +21,12 @@ const createTask = (evento) => {
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-
     
 
-
-    const content= `
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    
     //task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 
     
@@ -36,18 +37,4 @@ const createTask = (evento) => {
 
 btn.addEventListener('click', createTask);
 
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far","fa-check-square" ,"icon" );
-    i.addEventListener("click", completeTask);
-    return i ;
-}
 
-const completeTask = (event) => {
-    
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-}
-})() ;
